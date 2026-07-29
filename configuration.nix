2aa8b2243b5029ea3bde1c7de6ed9eb16b81063e
tommyrharper <thomasrobertharper@gmail.com>
2026-07-29 12:53:28 +0100

    feat: comment out macos settings i don't like

--- configuration.nix
+++ configuration.nix
@@ -15,15 +15,15 @@
   system.defaults = {
     NSGlobalDomain = {
       AppleInterfaceStyle = "Dark";
-      KeyRepeat = 2;          # fast key repeat
-      InitialKeyRepeat = 15;  # short delay before repeat
-      _HIHideMenuBar = true;  # auto-hide the menu bar
+      # KeyRepeat = 2;          # fast key repeat
+      # InitialKeyRepeat = 15;  # short delay before repeat
+      # _HIHideMenuBar = true;  # auto-hide the menu bar
       AppleShowAllExtensions = true;
     };
     dock.autohide = true;
-    finder.FXPreferredViewStyle = "Nlsv";  # list view by default
-    finder.CreateDesktop = false;          # clean desktop
-    trackpad.Clicking = true;              # tap to click
+    # finder.FXPreferredViewStyle = "Nlsv";  # list view by default
+    # finder.CreateDesktop = false;          # clean desktop
+    # trackpad.Clicking = true;              # tap to click
   };
   nix-homebrew = {
     enable = true;
