be8137260f254db7b4ff29f7e7ebf65d614b242a
tommyrharper <thomasrobertharper@gmail.com>
2026-07-29 12:53:37 +0100

    feat: add personal aliases

--- home.nix
+++ home.nix
@@ -37,6 +37,8 @@ in
       m = "git switch main";
       cc = "claude --dangerously-skip-permissions";
       co = "codex --full-auto";
+      cpath = "echo -n `pwd`|pbcopy";
+      gitverify="ssh-add /Users/${user}/.ssh/id_rsa"
     };
   };
 
