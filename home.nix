ab05af2f21c517dde98bb0cbaaa0f3114f10ac1b
Jakob <jakobsteen14@gmail.com>
2026-08-30 22:22:32 +0200

    Symlink tmux config directory via home-manager

--- home.nix
+++ home.nix
@@ -72,6 +72,8 @@ in
   # Edit-in-place: the real file stays in my repo, ~/.config just points at it.
   home.file.".config/wezterm".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/wezterm";
+  home.file.".config/tmux".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/tmux";
   home.file.".config/nvim".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/nvim";
   home.file.".config/herdr".source =
